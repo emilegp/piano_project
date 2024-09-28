37872595946538594da726c90d60d5c11f70404a
mldessu <marielou.324@gmail.com>
2024-09-28 15:43:22 -0400

    Code préliminaire training

--- piezo/notes_reference.py
+++ piezo/notes_reference.py
@@ -1,5 +1,5 @@
 import sounddevice as sd
-import csv
+import matplotlib.pyplot as plt
 import numpy as np
 
 seconds = 5 
@@ -23,7 +23,7 @@ if not default:
 
 recordings = []
 notes= ['c3','c-3','d3','d-3','e3','f3','f-3','g3','g-3','a3','a-3','b3']
-dt = 5e-7  #Intervalle de temps (en secondes)
+dt = 5e-2  #Intervalle de temps (en secondes)
 nb_points= int(dt*fs) #Équivalent en nombre de points pour les indices
 
 for note in notes:
@@ -32,7 +32,6 @@ for note in notes:
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels= 1)
     sd.wait()
     print(f'Enregistrement fini.')
-    print(myrecording)
 
     #Trouver l'amplitude maximale en valeur absolue
     max_amplitude = np.max(abs(myrecording))
@@ -40,28 +39,40 @@ for note in notes:
     print(threshold)
 
     #Créer la fenêtre utilisée pour le signal
+ 
     for index,value in enumerate(myrecording):
         if value>=threshold:
             start_signal=index
             break
-    cut_signal=myrecording[start_signal:start_signal+nb_points]
+
+    cut_signal=myrecording[start_signal:(start_signal+nb_points)].flatten()
 
     #Normalisation du signal
     norm_cut_signal=cut_signal/max_amplitude
+
+    t = np.linspace(0,dt,nb_points)
+    t2 = np.arange(0,5,1/44100)
+    print(len(norm_cut_signal))
+    print(len(t))
+
+    plt.plot(t, norm_cut_signal)
+    plt.xlabel('Temps [s]')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+    
+    plt.plot(t2, myrecording)
+    plt.xlabel('Temps [s]')
+    plt.ylabel('Amplitude')
+    plt.show()
 
     #Rajouter le nouveau array à la liste 'recordings'
     recordings.append(norm_cut_signal)
 
 #Transformer la liste en array
 recordings_array=np.array(recordings)
+print(recordings_array)
 
-print(recordings)
-#Créer le vecteur temps
-# t = np.arange(0,dt*1e6,nb_points)
 
 #Enregistrer un fichier csv
-np.savetxt('recordings.csv', recordings_array, delimiter=',')
-
-
- 
-
+np.savetxt('kwave\\reverse_runs\\recordings.csv', recordings_array, delimiter=',')
