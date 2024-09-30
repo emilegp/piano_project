9a22d05b5ee4a486b778a0130ada773f1010a2d1
Maxime-prog <maxime.rouillon@polymtl.ca>
2024-09-30 12:34:34 -0400

    Début Fonction Musiquer_un_input

---
+++ piezo/Musiquer_un_input.py
@@ -0,0 +1,56 @@
+# Objectif: Faire une fonction qui prend en input une valeur et 
+# sort de la musique
+
+
+#ChatGPT:
+# Étapes :
+# Installe pydub (si ce n'est pas déjà fait) avec la commande suivante :
+# pip install pydub 
+# Installe également ffmpeg ou libav pour permettre à pydub de lire des fichiers WAV. 
+# Tu peux télécharger et installer ffmpeg ici: https://ffmpeg.org/download.html.
+# Pour installer ffmpeg, installer chocolatey 
+#@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+# Une fois chocolatey installé, installer ffmpeg 
+# choco install ffmpeg
+# Organise tes fichiers WAV dans un répertoire en nommant chaque fichier selon la note (par exemple : C.wav, D.wav, etc.).
+# Crée la fonction en Python.
+
+from pydub.playback import play
+from pydub import AudioSegment
+from pydub.utils import which
+import os
+
+# Spécifie le chemin complet vers ffmpeg
+AudioSegment.converter = which("ffmpeg", path=r"C:\ProgramData\chocolatey\bin")
+
+
+# Dictionnaire associant les valeurs aux fichiers audio des notes
+notes_dict = {
+    'c3': 'c3.wav',
+    'd3': 'd3.wav',
+    'e3': 'e3.wav',
+    'f3': 'f3.wav',
+    'g3': 'g3.wav',
+    'a-3': 'a-3.wav',
+    'b3': 'b3.wav'
+}
+
+def jouer_note(valeur):
+    # Vérifier que la valeur est dans le dictionnaire
+    if valeur in notes_dict:
+        fichier_note = notes_dict[valeur]
+        
+        # Charger le fichier audio
+        note = AudioSegment.from_wav(fichier_note)
+        
+        # Jouer le fichier audio
+        play(note)
+    else:
+        print("Valeur non reconnue. Veuillez entrer une note valide.")
+
+# Exemple d'utilisation
+valeur = input("Entrez une note (C3, D3, E3, F3, G3, A3, B3) : ")
+jouer_note(valeur)
+
+
+
