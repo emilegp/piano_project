3865b85d10b1a189477c60c3565353de71775fb2
mldessu <marielou.324@gmail.com>
2024-10-03 12:48:33 -0400

    Training notes

--- piezo/notes_reference.py
+++ piezo/notes_reference.py
@@ -1,8 +1,8 @@
 import sounddevice as sd
-import matplotlib.pyplot as plt
 import numpy as np
+import json
 
-seconds = 5 
+seconds = 1 
 fs = 44100      # Sampling rate    
 
 default = True #Si cette option est utilisée, le micro/speaker par défaut est utilisé
@@ -21,34 +21,44 @@ if not default:
 
     sd.default.device = [int(DeviceIn), int(DeviceOut)]
 
-recordings = []
 notes= ['c3','c-3','d3','d-3','e3','f3','f-3','g3','g-3','a3','a-3','b3']
-dt = 5e-2  #Intervalle de temps (en secondes)
+notes_dict = {note: None for note in notes}
+print(notes_dict)
+dt = 1e-1  #Intervalle de temps (en secondes)
 nb_points= int(dt*fs) #Équivalent en nombre de points pour les indices
 
 for note in notes:
-    print(f'Enregistrement de la note',note)
+    recordings=[]
+    for i in range(5):
+        print(f'Enregistrement de la note',note)
 
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels= 1)
-    sd.wait()
-    print(f'Enregistrement fini.')
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels= 1)
+        sd.wait()
+        print(f'Enregistrement fini.')
 
-    #Trouver l'amplitude maximale en valeur absolue
-    max_amplitude = np.max(abs(myrecording))
-    threshold = max_amplitude / 10  #Définir le seuil
-    print(threshold)
+        #Trouver l'amplitude maximale en valeur absolue
+        max_amplitude = np.max(abs(myrecording))
+        threshold = max_amplitude / 10  #Définir le seuil
+        print(threshold)
 
-    #Créer la fenêtre utilisée pour le signal
- 
-    for index,value in enumerate(myrecording):
-        if value>=threshold:
-            start_signal=index
-            break
+        #Créer la fenêtre utilisée pour le signal
+    
+        for index,value in enumerate(myrecording):
+            if value>=threshold:
+                start_signal=index
+                break
 
-    cut_signal=myrecording[start_signal:(start_signal+nb_points)].flatten()
+        cut_signal=myrecording[start_signal:(start_signal+nb_points)].flatten()
 
-    #Normalisation du signal
-    norm_cut_signal=cut_signal/max_amplitude
+        #Normalisation du signal
+        norm_cut_signal=cut_signal/max_amplitude
+
+        #Rajouter le nouveau array à la liste 'recordings'
+        recordings.append(norm_cut_signal.tolist())
+
+    #Transformer la liste en array
+    nom_note=note
+    notes_dict[nom_note]=recordings
 
     # t = np.linspace(0,dt,nb_points)
     # t2 = np.arange(0,5,1/44100)
@@ -62,13 +72,8 @@ for note in notes:
     # plt.plot(t2, myrecording)
     # plt.xlabel('Temps [s]')
     # plt.ylabel('Amplitude')
-    # plt.show()
+    # plt.show()  
 
-    #Rajouter le nouveau array à la liste 'recordings'
-    recordings.append(norm_cut_signal)
-
-#Transformer la liste en array
-recordings_array=np.array(recordings)
-
-#Enregistrer un fichier csv
-np.savetxt('piezo\\recordings.csv', recordings_array, delimiter=',')
+# Save to a JSON file
+with open('piezo\\notes_dict.json', 'w') as json_file:
+    json.dump(notes_dict, json_file)
